e57715026446d72b587df20345dc132575ac46d8
jameszu <zqxhjjh@outlook.com>
2020-09-16 11:28:13 +1200

    update

--- CanvasAPI/CanvasAPI/Controllers/PollController.cs
+++ CanvasAPI/CanvasAPI/Controllers/PollController.cs
@@ -25,5 +25,77 @@ namespace CanvasAPI.Controllers
         {
             return await _context.Poll.ToListAsync();
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Poll>> GetPoll(int id)
+        {
+            var the_poll = await _context.Poll.FindAsync(id);
+
+            if (the_poll == null)
+            {
+                return NotFound();
+            }
+
+            return the_poll;
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> CreatePoll(Poll poll)
+        {
+            _context.Poll.Add(poll);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetPoll), new { id = poll.Poll_ID }, poll);
+        }
+
+        [HttpPut("Id")]
+        //[Route("UpdateName")]
+        public async Task<IActionResult> PutName(int id, Poll poll)
+        {
+            if (id != poll.Poll_ID)
+            {
+                return BadRequest();
+            }
+
+            var updatePoll = await _context.Poll.FirstOrDefaultAsync(s => s.Pool_Title == poll.Pool_Title);
+            _context.Entry(updatePoll).State = EntityState.Modified;
+
+            updatePoll.Pool_Title = poll.Pool_Title;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PollExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        private bool PollExist(int id)
+        {
+            return _context.Poll.Any(e => e.Poll_ID == id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Poll>> DeletePoll(int id)
+        {
+            var poll = await _context.Poll.FindAsync(id);
+            if (poll == null)
+            {
+                return NotFound();
+            }
+
+            _context.Poll.Remove(poll);
+            await _context.SaveChangesAsync();
+
+            return poll;
+        }
     }
 }
