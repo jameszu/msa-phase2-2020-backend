decb10ed1ab5bf056b5ba9efa65d400ec333672a
jameszu <zqxhjjh@outlook.com>
2020-09-17 22:32:41 +1200

    updatedatabase

---
+++ CanvasAPI/CanvasAPI/Migrations/20200917103150_InitialCreate-final.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CanvasAPI.Migrations
+{
+    public partial class InitialCreatefinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
